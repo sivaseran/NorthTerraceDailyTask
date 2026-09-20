11693e4cadec40d23edf8d08c09abb9724316624
Seran Sivananthamoorthy <5sivas01@solent.ac.uk>
2026-09-21 00:22:01 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE='north-terrace-v3-2-3-final';
+const CACHE='north-terrace-v3-6-final';
 const ASSETS=[
   './','./index.html','./login.html','./staff.html','./manager.html','./css/style.css',
   './js/firebase-config.js','./js/firebase.js','./js/auth.js','./js/store.js','./js/ui.js',
   './js/general.js','./js/staff.js','./js/manager.js','./js/reports.js','./js/seed.js',
-  './js/login.js','./manifest.json'
+  './js/login.js','./js/final-config.js','./manifest.json'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
